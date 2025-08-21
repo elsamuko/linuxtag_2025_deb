818507c7e089a573b4fc94222cfc7aebdd0e8563
Samuel Albrecht <elsamuko@gmail.com>
2025-08-21 16:40:58 +0200

    add: demo for packaging a uv project

---
+++ dh_python/make_deb.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+# python3 -m pip install uv --b
+# sudo apt install python3-hatchling
+
+if [ -d linuxtag ]; then
+    rm -rf linuxtag*
+fi
+
+uv init --package linuxtag --build-backend hatch
+(
+    cd linuxtag || exit
+    uv add requests
+)
+
+mkdir -p linuxtag/debian
+
+cat << EOF > linuxtag/debian/control
+Source: linuxtag
+Priority: optional
+Section: misc
+Maintainer: Samuel <elsamuko@gmail.com>
+Build-Depends: debhelper-compat (= 13),
+               dh-python,
+               python3-all,
+               python3-setuptools,
+               python3-build,
+               pybuild-plugin-pyproject
+
+Package: linuxtag
+Depends: \${python3:Depends}, \${misc:Depends}
+Architecture: all
+Description: linuxtagnapp
+ prints linuxtag
+EOF
+
+cat << EOF > linuxtag/debian/rules
+#!/usr/bin/make -f
+
+%:
+	dh \$@ --with python3 --buildsystem=pybuild
+EOF
+
+cat << EOF > linuxtag/debian/changelog
+linuxtag (1.0-1) unstable; urgency=medium
+
+  * Initial release.
+
+ -- Samuel <elsamuko@gmail.com>  Wed, 20 Aug 2025 15:32:02 +0200
+EOF
+
+cat << EOF > linuxtag/debian/copyright
+Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+Files:
+ *
+Copyright: 2024-$(date +%Y) elsamuko
+License: Proprietary
+EOF
+
+(
+    cd linuxtag || exit
+    dpkg-buildpackage -us -uc
+)
+
+dpkg-deb --info linuxtag*.deb
+dpkg-deb --contents linuxtag*.deb
